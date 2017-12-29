1b90281a56ff7b6fae4518f2499be721f6b52221
ihack4falafel <hashem.jawad@yahoo.com>
2017-12-29 08:35:22 -0500

    Update HelloWorld.asm

--- HelloWorld.asm
+++ HelloWorld.asm
@@ -5,21 +5,21 @@
 global _start
 
 
-; section for code
+; code goes here
 section .text
 
 _start:
 
-; System call for write /usr/include/i386-linux-gnu/asm/unistd_32.h | #define __NR_write  4
+; system call for write /usr/include/i386-linux-gnu/asm/unistd_32.h | #define __NR_write  4
 
-	; Print "Hello World" to Screen
+	; print "Hello World" to screen
 	; mov instruction moves data between destination and source (move dest, source)
         ; ssize_t write(int fd, const void *buf, size_t count);
         ; syscall for write instruction
 	; file descriptor for stdout 1
-	; store pointer value to "Hello World!" string
+	; store pointer value to "Hello World!" string, used message lable
 	; store string lengnth
-	; go ahead and do system call
+	; go ahead and interrupt to do system call
 
 	mov eax, 0x4
 	mov ebx, 0x1
@@ -27,23 +27,24 @@ _start:
 	mov edx, mlen
 	int 0x80
 
-; System call for exit  /usr/include/i386-linux-gnu/asm/unistd_32.h | #define __NR_exit   1
+; system call for exit  /usr/include/i386-linux-gnu/asm/unistd_32.h | #define __NR_exit   1
 
 	; exit program gracefully
 	; void exit(int status);
 	; random exit code
-	; go ahead and do system call
+	; go ahead and interrupt to do system call
 
 	mov eax, 0x1
 	mov ebx, 0x5
 	int 0x80 
 
-; section for initilized data
+; initilized data goes here
 section .data
 
-	; Message Label
+	; message Label
 	; mlen stands for message length
 	; equ stands for equal
+	; db stands for define byte
 
 	message: db "Hello World!"
 	mlen   equ $-message
